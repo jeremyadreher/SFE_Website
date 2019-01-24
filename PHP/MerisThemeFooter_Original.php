e44839db00c3b0357f3322555d3d20401e170e3d
Jeremy Dreher <jeremyadreher@gmail.com>
2019-01-24 10:12:00 -0800

    Create MerisThemeFooter_Original.php

---
+++ PHP/MerisThemeFooter_Original.php
@@ -0,0 +1,16 @@
+<!--Footer-->
+<footer>
+			<div class="container text-center">
+				<div class="site-sns">
+               <a href = "https://www.facebook.com/SFE.Inc" target = "_blank"><i class = "fa fa-facebook"></i></a><a href = "https://twitter.com/Silicon_Forest" target = "_blank"><i class = "fa fa-twitter"></i></a><a href = "https://www.linkedin.com/company/107954?trk=prof-exp-company-name" target = "_blank"><i class = "fa fa-linkedin"></i></a>
+					
+				</div>
+				<div class="site-info">
+					    <br />Secure <a href="https://mail.si-forest.com/owa" target="_blank" style="color:#000;">Email</a>
+				</div>
+			</div>
+		</footer>
+	</div>	
+    <?php wp_footer();?>
+</body>
+</html>
