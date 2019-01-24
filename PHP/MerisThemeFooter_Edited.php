e2bfdd7b6235f7b6fc3aba99e46f05ba30c2f895
Jeremy Dreher <jeremyadreher@gmail.com>
2019-01-24 11:04:17 -0800

    Update MerisThemeFooter_Edited.php

--- PHP/MerisThemeFooter_Edited.php
+++ PHP/MerisThemeFooter_Edited.php
@@ -6,8 +6,8 @@
             <a href = "https://www.linkedin.com/company/107954?trk=prof-exp-company-name" target = "_blank"><i class = "fa fa-linkedin"></i></a>		
 		</div>
 		<div class="site-info">
-			Secure <a href="https://mail.si-forest.com/owa" target="_blank" style="color:#000;">Email</a>
-			<a href="https://www.siliconforestelectronics.com/wp-content/uploads/2019/01/SFE_PrivacyPolicy.pdf" target="_blank"> Privacy Policy </a>
+			<!-- Secure <a href="https://mail.si-forest.com/owa" target="_blank" style="color:#000;">Email</a> -->
+			<a href="https://www.siliconforestelectronics.com/wp-content/uploads/2019/01/SFE_PrivacyPolicy.pdf" target="_blank" style="color:#000"> Privacy Policy </a>
 		</div>
 	</div>
 </footer>
